f7548dd928c888fc68223d390342cc4d804e8d61
PedroszSantana <pedrosouza-s@hotmail.com>
2023-06-14 12:57:40 -0300

    Rename Folder

---
+++ src/components/Home/Animation/Animation.tsx
@@ -0,0 +1,15 @@
+import Gif from '../../../assets/images/d7b3da3d4b19b7f0c44673374158df0a.gif'
+import { AnimationGif } from './StyledAnimation'
+
+export const Animation = () => {
+  return (
+    <div style={{ overflowX: 'hidden' }}>
+      <AnimationGif Conteiner="up">
+        <img className="gif_bnc animationgif" src={Gif} alt="" />
+      </AnimationGif>
+      <AnimationGif Conteiner="bottom">
+        <img className="gif_bnc gif_bnc2 animationgif2" src={Gif} alt="" />
+      </AnimationGif>
+    </div>
+  )
+}
